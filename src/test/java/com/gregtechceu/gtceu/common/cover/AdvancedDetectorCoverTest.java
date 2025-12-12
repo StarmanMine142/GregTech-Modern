eafea6d11afa9ff03c4ccc19ec85263ec7ced776
Gustavo <ofoxsmith@outlook.com>
2025-12-12 11:59:02 +1100

    spotless stuff, create IGregtechBlockEntity

--- src/test/java/com/gregtechceu/gtceu/common/cover/AdvancedDetectorCoverTest.java
+++ src/test/java/com/gregtechceu/gtceu/common/cover/AdvancedDetectorCoverTest.java
@@ -1,7 +1,6 @@
 package com.gregtechceu.gtceu.common.cover;
 
 import com.gregtechceu.gtceu.GTCEu;
-import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.common.cover.detector.AdvancedFluidDetectorCover;
 import com.gregtechceu.gtceu.common.cover.detector.AdvancedItemDetectorCover;
